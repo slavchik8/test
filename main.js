5b7ed203dd5453535bbc6b7a4c00b047d4bcc78d
Vyacheslav <vyacheslav.pankov.97@mail.ru>
2019-06-17 18:05:52 +0500

    This is my first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($) {
+	
+
+	/******************************************************************************************************/
+	/**/
+	let person = {
+		name: 'Elizabet',
+		age: 22,
+		isMarried: false
+	}
+	console.log(person.name);
+
+	let arr = ["first.png", "last.png", "nth.png"];
+	console.log(arr[1]);
+
+});
